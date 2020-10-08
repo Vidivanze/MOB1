bfe2ba858cabd2e13344cf703a4000e827420567
Yvann <yvann.butticaz@gmail.com>
2020-10-08 16:09:26 +0200

    Change api service by an http interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,16 +9,22 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { HttpClientModule } from '@angular/common/http';
+import { DataProvider } from './providers/data';
+import { HTTP_INTERCEPTORS, HttpClient, HttpClientModule} from '@angular/common/http';
+import { ApiInterceptor } from './interceptor/ApiInterceptor';
+import { IonicStorageModule } from '@ionic/storage';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, IonicStorageModule.forRoot()],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    { provide: HTTP_INTERCEPTORS, useClass: ApiInterceptor, multi: true },
+    DataProvider,
+    HttpClient
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/providers/data.ts
@@ -0,0 +1,29 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {reject, resolve} from 'q';
+import { Storage } from '@ionic/storage';
+
+@Injectable()
+export class DataProvider {
+  
+  private apiurl = "http://127.0.0.1:8000/api";
+
+  public stock = []
+  public user = []
+
+  constructor(private http: HttpClient, private storage: Storage){}
+
+  public loadFromAPI(): Promise<any>
+  {
+    return new Promise<any> ( (resolve, reject) => {
+      this.http.get(this.apiurl+'/products').subscribe(
+        response => {
+          this.stock = response['data'];
+        },
+        err => {
+          console.log('API failed with code '+err.status)
+        }
+      )
+    })
+  }
+}
